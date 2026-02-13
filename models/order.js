138f1aec441950750eb38450fd844e29a010e79b
yonal5 <yonalabasingha@gmail.com>
2026-02-13 14:41:53 +0530

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1,69 +1,58 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema(
-    {
-        orderID : {
-            type : String,
-            required : true,
-            unique : true
-        },
-        items : {
-            type : [
-                {
-                    productID : {
-                        type : String,
-                        required : true
-                    },
-                    quantity : {
-                        type : Number,
-                        required : true
-                    },
-                    name : {
-                        type : String,
-                        required : true
-                    },
-                    price : {
-                        type : Number,
-                        required : true
-                    },
-                    image : {
-                        type : String,
-                        required : true
-                    }
-                }
-            ]
-        },
-        customerName : {
-            type : String,
-            required : true
-        },
-        email : {
-            type : String,
-            required : true
-        },
-        phone : {
-            type : String,
-            required : true
-        },
-        address : {
-            type : String,
-            required : true
-        },
-        total : {
-            type : Number,
-            required : true
-        },
-        status : {
-            type : String,
-            required : true,
-            default : "pending"
-        },
-        date : {
-            type : Date,
-            default : Date.now
-        }
-    }
-)
+const orderSchema = new mongoose.Schema({
 
-const Order = mongoose.model("Order", orderSchema)
-export default Order
+    orderID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    items: [
+        {
+            productID: String,
+            quantity: Number,
+            name: String,
+            price: Number,
+            image: String
+        }
+    ],
+
+    customerName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    phone: {
+        type: String,
+        required: true
+    },
+
+    address: {
+        type: String,
+        required: true
+    },
+
+    total: {
+        type: Number,
+        required: true
+    },
+
+    status: {
+        type: String,
+        default: "pending"
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+export default mongoose.model("Order", orderSchema);
